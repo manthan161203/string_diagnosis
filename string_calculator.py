31d1f956d112435f672790cfd32348642149e07b
manthan161203 <manthanlp3204@gmail.com>
2024-08-01 18:12:09 +0530

    Support custom delimiters

--- string_calculator.py
+++ string_calculator.py
@@ -1,6 +1,11 @@
 def add(numbers: str) -> int:
     if numbers == "":
         return 0
-    numbers = numbers.replace('\n', ',')
-    num_list = numbers.split(',')
+    delimiter = ','
+    if numbers.startswith("//"):
+        parts = numbers.split('\n', 1)
+        delimiter = parts[0][2:]
+        numbers = parts[1]
+    numbers = numbers.replace('\n', delimiter)
+    num_list = numbers.split(delimiter)
     return sum(int(num) for num in num_list)
--- test_string_calculator.py
+++ test_string_calculator.py
@@ -19,6 +19,9 @@ class TestStringCalculator(unittest.TestCase):
     def test_new_line_delimiter(self):
         self.assertEqual(add("1\n2,3"), 6)
 
-    
+    def test_custom_delimiter(self):
+        self.assertEqual(add("//;\n1;2"), 3)
+        self.assertEqual(add("//|\n1|2|3"), 6)
+
 if __name__ == "__main__":
     unittest.main()
